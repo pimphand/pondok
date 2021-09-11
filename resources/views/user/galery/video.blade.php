eaad052eb278ed8ada800dda1e20d3c95c85ae11
Aldiansyah <aldiansyahunip@gmail.com>
2021-09-11 11:02:13 +0700

    update video

--- resources/views/user/galery/video.blade.php
+++ resources/views/user/galery/video.blade.php
@@ -17,7 +17,70 @@
     </div>
 </div>
 
-<div class="gdlr-core-pbf-wrapper" style="padding: 100px 20px 30px 20px;">
+<div class="kingster-page-wrapper" id="kingster-page-wrapper">
+    <div class="gdlr-core-page-builder-body">
+        <div class="gdlr-core-pbf-wrapper" style="padding: 50px 0px 30px 0px;">
+            <div class="gdlr-core-pbf-background-wrap"></div>
+            <div class="gdlr-core-pbf-wrapper-content gdlr-core-js">
+                <div class="clearfix gdlr-core-pbf-wrapper-container gdlr-core-container">
+                    <div class="gdlr-core-pbf-element">
+
+                        <div class="clearfix gdlr-core-portfolio-item gdlr-core-item-pdb gdlr-core-portfolio-item-style-grid" style="padding-bottom: 10px;">
+                            <div class="clearfix gdlr-core-portfolio-item-holder gdlr-core-js-2 filter-container" data-layout="masonry">
+                                @foreach ($data as $item)
+                                @if (!$item->video == null)
+                                <div class="gdlr-core-item-list gdlr-core-item-pdlr class3 gdlr-core-column-15 ">
+                                    <div class="gdlr-core-portfolio-grid gdlr-core-left-align gdlr-core-style-normal">
+                                        <div class="gdlr-core-portfolio-thumbnail gdlr-core-media-image gdlr-core-style-title-icon">
+                                            <div class="gdlr-core-portfolio-thumbnail-image-wrap gdlr-core-zoom-on-hover">
+                                                <video width="300" height="195" controls>
+                                                    <source src="{{ asset('storage/video') }}/{{ $item->video }}"
+                                                        type="video/mp4">
+                                                    Browhser anda tidak didukung untuk memutar video ini
+                                                </video>
+                                            </div>
+                                        </div>
+                                        <div class="gdlr-core-portfolio-content-wrap gdlr-core-skin-divider">
+                                            <h3 class="gdlr-core-portfolio-title gdlr-core-skin-title" style="font-size: 21px; font-weight: 600; letter-spacing: 0px; text-transform: none;">
+                                                <a href="#">{{$item->name}}</a>
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                @else
+                                <div class="gdlr-core-item-list gdlr-core-item-pdlr class3 gdlr-core-column-15 ">
+                                    <div class="gdlr-core-portfolio-grid gdlr-core-left-align gdlr-core-style-normal">
+                                        <div class="gdlr-core-portfolio-thumbnail gdlr-core-media-image gdlr-core-style-title-icon">
+                                            <div class="gdlr-core-portfolio-thumbnail-image-wrap gdlr-core-zoom-on-hover">
+                                                <iframe width="800" height="550"
+                                            src="https://www.youtube.com/embed/{{ $item->link }}"
+                                            title="YouTube video player" frameborder="0"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                            allowfullscreen>
+                                        </iframe>
+                                            </div>
+                                        </div>
+                                        <div class="gdlr-core-portfolio-content-wrap gdlr-core-skin-divider">
+                                            <h3 class="gdlr-core-portfolio-title gdlr-core-skin-title" style="font-size: 21px; font-weight: 600; letter-spacing: 0px; text-transform: none;">
+                                                <a href="#">{{$item->name}}</a>
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                @endif
+                                @endforeach
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+{{-- <div class="gdlr-core-pbf-wrapper" style="padding: 100px 20px 30px 20px;">
     <div class="gdlr-core-pbf-background-wrap"></div>
     <div class="gdlr-core-pbf-wrapper-content gdlr-core-js">
         <div class="clearfix gdlr-core-pbf-wrapper-container gdlr-core-container">
@@ -93,6 +156,6 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 
 @endsection
--- resources/views/user/home/video.blade.php
+++ resources/views/user/home/video.blade.php
@@ -42,6 +42,101 @@
         </div>
     </div>
 </div>
+
+<div class="gdlr-core-pbf-wrapper gdlr-core-hide-in-tablet-mobile"
+    style="margin: -448px 0px 0px 0px; padding: 0px 0px 90px 0px;" id="gdlr-core-wrapper-7">
+    <div class="gdlr-core-pbf-background-wrap"></div>
+    <div class="gdlr-core-pbf-wrapper-content gdlr-core-js">
+        <div class="clearfix gdlr-core-pbf-wrapper-container gdlr-core-container">
+            <div class="gdlr-core-pbf-column gdlr-core-column-60 gdlr-core-column-first" id="gdlr-core-column-37730"
+                style="z-index: 9;">
+                <div class="gdlr-core-pbf-column-content-margin gdlr-core-js gdlr-core-item-mglr gdlr-core-item-mgb"
+                    style="margin: 0px 0px 0px 0px; padding-top: 50px; padding-bottom: 0px;">
+                    <div class="gdlr-core-pbf-background-wrap"></div>
+                    <div class="clearfix gdlr-core-pbf-column-content gdlr-core-js">
+                        <div class="gdlr-core-pbf-element">
+                            <div class="clearfix gdlr-core-blog-item gdlr-core-item-pdb gdlr-core-style-blog-column-with-frame gdlr-core-item-pdlr"
+                                style="padding-bottom: 0px;">
+                                <div class="gdlr-core-flexslider flexslider gdlr-core-js-2 gdlr-core-bullet-style-round3 gdlr-core-color-bullet"
+                                    data-type="carousel" data-column="2" data-move="1" data-nav="bullet"
+                                    data-nav-parent="gdlr-core-blog-item" data-controls-top-margin="70px">
+                                    <ul class="slides">
+                                        @foreach ($video as $item)
+                                        @if (!$item->video == null)
+                                        <li class="gdlr-core-item-mglr">
+                                            <div class="gdlr-core-blog-grid gdlr-core-style-4 gdlr-core-js gdlr-core-left-button gdlr-core-blog-grid-with-frame gdlr-core-item-mgb gdlr-core-skin-e-background gdlr-core-outer-frame-element gdlr-core-with-thumbnail"
+                                                style="box-shadow: 0 20px 35px rgba(10, 10, 10, 0.1); -moz-box-shadow: 0 20px 35px rgba(10, 10, 10, 0.1); -webkit-box-shadow: 0 20px 35px rgba(10, 10, 10, 0.1);"
+                                                data-sync-height="blog-item-1">
+                                                <div
+                                                    class="gdlr-core-blog-thumbnail gdlr-core-media-image gdlr-core-opacity-on-hover">
+                                                    <a href="#">
+                                                        <video width="615" height="400" controls>
+                                                            <source src="{{ asset('storage/video') }}/{{ $item->video }}"
+                                                                type="video/mp4">
+                                                            Browhser anda tidak didukung untuk memutar video ini
+                                                        </video>
+                                                    </a>
+                                                </div>
+                                                <div
+                                                    class="clearfix gdlr-core-blog-grid-frame gdlr-core-sync-height-space-position">
+                                                    <div class="clearfix gdlr-core-blog-grid-top-info">
+                                                        <span
+                                                            class="gdlr-core-blog-info gdlr-core-blog-info-font gdlr-core-skin-caption gdlr-core-blog-info-date">
+                                                            <a href="#">{{ Date::parse($item->created_at)->format('j F Y') }}</a>
+                                                        </span>
+                                                    </div>
+                                                    <h3 class="gdlr-core-blog-title gdlr-core-skin-title"
+                                                        style="font-size: 24px; font-weight: 500; letter-spacing: 1px;">
+                                                        <a href="#">{{ $item->name }}</a>
+                                                    </h3>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        @else
+                                        <li class="gdlr-core-item-mglr">
+                                            <div class="gdlr-core-blog-grid gdlr-core-style-4 gdlr-core-js gdlr-core-left-button gdlr-core-blog-grid-with-frame gdlr-core-item-mgb gdlr-core-skin-e-background gdlr-core-outer-frame-element gdlr-core-with-thumbnail"
+                                                style="box-shadow: 0 20px 35px rgba(10, 10, 10, 0.1); -moz-box-shadow: 0 20px 35px rgba(10, 10, 10, 0.1); -webkit-box-shadow: 0 20px 35px rgba(10, 10, 10, 0.1);"
+                                                data-sync-height="blog-item-1">
+                                                <div
+                                                    class="gdlr-core-blog-thumbnail gdlr-core-media-image gdlr-core-opacity-on-hover">
+                                                    <a href="#">
+                                                        <iframe width="700" height="450"
+                                                            src="https://www.youtube.com/embed/{{ $item->link }}"
+                                                            title="YouTube video player" frameborder="0"
+                                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                                            allowfullscreen>
+                                                        </iframe>
+                                                    </a>
+                                                </div>
+                                                <div
+                                                    class="clearfix gdlr-core-blog-grid-frame gdlr-core-sync-height-space-position">
+                                                    <div class="clearfix gdlr-core-blog-grid-top-info">
+                                                        <span
+                                                            class="gdlr-core-blog-info gdlr-core-blog-info-font gdlr-core-skin-caption gdlr-core-blog-info-date">
+                                                            <a href="#">{{ Date::parse($item->created_at)->format('j F Y') }}</a>
+                                                        </span>
+                                                    </div>
+                                                    <h3 class="gdlr-core-blog-title gdlr-core-skin-title"
+                                                        style="font-size: 24px; font-weight: 500; letter-spacing: 1px;">
+                                                        <a href="#">{{ $item->name }}</a>
+                                                    </h3>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        @endif
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{--
 <div class="gdlr-core-pbf-wrapper" style="margin-top: -258px; padding: 0px 0px 100px 0px;">
     <div class="gdlr-core-pbf-background-wrap"></div>
     <div class="gdlr-core-pbf-wrapper-content gdlr-core-js">
@@ -90,11 +185,11 @@
                                             <div class="gdlr-core-event-item-thumbnail">
                                                 <a href="#">
                                                     <iframe width="700" height="450"
-                                            src="https://www.youtube.com/embed/{{ $item->link }}"
-                                            title="YouTube video player" frameborder="0"
-                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                            allowfullscreen>
-                                        </iframe>
+                                                        src="https://www.youtube.com/embed/{{ $item->link }}"
+                                                        title="YouTube video player" frameborder="0"
+                                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                                        allowfullscreen>
+                                                    </iframe>
                                                 </a>
                                             </div>
                                             <div class="gdlr-core-frame gdlr-core-skin-e-background gdlr-core-js" data-sync-height="event-item-1">
@@ -120,6 +215,6 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 
 
